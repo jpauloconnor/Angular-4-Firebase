a7c7010e80198eaa3b84a71cafd212d708155087
Paul O'Connor <jamespauloconnor@gmail.com>
2017-08-03 21:04:53 -0400

    Initial repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-savCHgLMRQA1IPWl2NJe1IGyoWma8Tk",
+    authDomain: "todo-82aae.firebaseapp.com",
+    databaseURL: "https://todo-82aae.firebaseio.com",
+    projectId: "todo-82aae",
+    storageBucket: "todo-82aae.appspot.com",
+    messagingSenderId: "42987731881"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
